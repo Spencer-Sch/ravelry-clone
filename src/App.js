c81a7550071e55298c770f3e7e17b45f1aa3be5e
Spencer Schoeneman <spencer.schoeneman@gmail.com>
2021-10-28 09:51:43 -0500

    added SiteUi component to house NavBar and UserBar components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import './App.css';
-import NavBar from './components/NavBar';
-import UserBar from './components/UserBar';
+import SiteUi from './components/SiteUi';
 
 const App = () => {
-  return (
-    <>
-      <NavBar />
-      <UserBar />
-    </>
-  );
+  return <SiteUi />;
 };
 
 export default App;
---
+++ src/components/SiteUi.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from './NavBar';
+import UserBar from './UserBar';
+
+const SiteUi = () => {
+  return (
+    <>
+      <NavBar />
+      <UserBar />
+    </>
+  );
+};
+
+export default SiteUi;
