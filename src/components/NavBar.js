93531e6690d200245396cb3872d671cd7e73a750
Spencer Schoeneman <spencer.schoeneman@gmail.com>
2021-10-28 09:28:07 -0500

    added buttons, positioned buttons, added styles to NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,54 @@
-import { AppBar, IconButton, Toolbar, Typography } from '@material-ui/core';
+import {
+  AppBar,
+  Grid,
+  IconButton,
+  makeStyles,
+  Toolbar,
+  Typography,
+} from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+
+const useStyle = makeStyles({
+  toolbar: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  h1: {
+    textTransform: 'uppercase',
+    fontSize: '3rem',
+    padding: '0.5rem',
+  },
+  buttonDiv: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+});
 
 const NavBar = () => {
+  const classes = useStyle();
+
   return (
     <AppBar color="primary" position="static">
-      <Toolbar>
-        <Typography variant="h4">RAVELRY</Typography>
-        <IconButton edge="start" color="inherit" aria-label="menu">
-          <MenuIcon />
-        </IconButton>
+      <Toolbar className={classes.toolbar}>
+        <Grid container xs={11}>
+          <Typography variant="h1" className={classes.h1}>
+            Skein Queen
+          </Typography>
+        </Grid>
+        <Grid
+          container
+          xs={1}
+          justifyContent="space-evenly"
+          clasName={classes.buttonGrid}
+        >
+          <IconButton edge="end" color="inherit" aria-label="menu">
+            <MenuIcon fontSize="large" />
+          </IconButton>
+          <IconButton edge="end" color="inherit" aria-label="menu">
+            <AccountCircleIcon fontSize="large" />
+          </IconButton>
+        </Grid>
       </Toolbar>
     </AppBar>
   );
