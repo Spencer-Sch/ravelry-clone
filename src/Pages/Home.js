7d79166a41b08c401cf792b53503c9ad4ac4d64d
Spencer Schoeneman <spencer.schoeneman@gmail.com>
2021-10-28 10:12:06 -0500

    added Pages folder. added Home and Project pages

---
+++ src/Pages/Home.js
@@ -0,0 +1,12 @@
+import { Typography } from '@material-ui/core';
+import React from 'react';
+
+const Home = () => {
+  return (
+    <>
+      <Typography variant="h1">Home Page</Typography>
+    </>
+  );
+};
+
+export default Home;
---
+++ src/Pages/Projects.js
@@ -0,0 +1,12 @@
+import { Typography } from '@material-ui/core';
+import React from 'react';
+
+const Projects = () => {
+  return (
+    <>
+      <Typography variant="h1">Projects Page</Typography>
+    </>
+  );
+};
+
+export default Projects;
