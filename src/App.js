95ed2ce2f8df88cd7a49e22f344fe3c750abb9db
Spencer Schoeneman <spencer.schoeneman@gmail.com>
2021-10-28 10:11:20 -0500

    added routing to app and index files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
 import SiteUi from './components/SiteUi';
+import Home from './Pages/Home';
+import Projects from './Pages/Projects';
 
 const App = () => {
-  return <SiteUi />;
+  return (
+    <>
+      <SiteUi />
+      <main>
+        <Route path="/home">
+          <Home />
+        </Route>
+        <Route path="/projects">
+          <Projects />
+        </Route>
+      </main>
+    </>
+  );
 };
 
 export default App;
