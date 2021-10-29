39c4ef010de781be0f3054f4cfca9f5c217d4542
Spencer Schoeneman <spencer.schoeneman@gmail.com>
2021-10-29 16:07:51 -0500

    added NewProjectModal component
    This modal will be where the user fills out the form for a new project.
    
    I took the demo code from the material-ui documentation and slightly
    altered it to get it to work on the Projects page. I have yet to add
    any of the actual form inputs and styling.

---
+++ src/components/NewProjectModal.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import { Slide } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
+const NewProjectModal = (props) => {
+  const classes = useStyles();
+  //   const [show, setShow] = useState(false);
+
+  //   const handleOpen = () => {
+  //     setShow(true);
+  //   };
+
+  //   const handleClose = () => {
+  //     setShow(false);
+  //   };
+
+  return (
+    <>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={props.open}
+        onClose={props.onClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Slide direction="left" in={props.open}>
+          <div className={classes.paper}>
+            <h2 id="transition-modal-title">Transition modal</h2>
+            <p id="transition-modal-description">
+              react-transition-group animates me.
+            </p>
+          </div>
+        </Slide>
+      </Modal>
+    </>
+  );
+};
+
+export default NewProjectModal;
