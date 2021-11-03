4a14f1effbe913967aa3a006f69d434b6f4d4370
Spencer Schoeneman <spencer.schoeneman@gmail.com>
2021-11-03 06:17:51 -0500

    added image placeholder on ProjectDetails page. added some layout styling

--- src/Pages/ProjectDetails.js
+++ src/Pages/ProjectDetails.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import { Button, Grid, makeStyles, Typography } from '@material-ui/core';
+import { Button, Grid, makeStyles, Paper, Typography } from '@material-ui/core';
 import EditIcon from '@material-ui/icons/Edit';
 import DeleteIcon from '@material-ui/icons/Delete';
 import AddToPhotosIcon from '@material-ui/icons/AddToPhotos';
+import InsertPhotoIcon from '@material-ui/icons/InsertPhoto';
 import { DUMMY_DATA } from '../constants/dummy_data';
 
 const useStyles = makeStyles((theme) => ({
@@ -20,8 +21,29 @@ const useStyles = makeStyles((theme) => ({
   },
   contentgrid: {},
   detailsGrid: {},
-  detailsTitleGrid: {},
+  detailsTitleGrid: {
+    marginBottom: theme.spacing(3),
+  },
   h3: {},
+  imageContainerGrid: {
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(3),
+  },
+  imageGrid: {
+    width: '100%',
+  },
+  imagePaper: {
+    border: 'solid 2px black',
+    height: '10rem',
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    cursor: 'pointer',
+  },
+  imageIcon: {
+    fontSize: '3rem',
+  },
   detailsItemGrid: {
     borderBottom: 'solid 1px #ddd',
     marginBottom: '1rem',
@@ -32,8 +54,10 @@ const useStyles = makeStyles((theme) => ({
     textTransform: 'uppercase',
   },
   detailsItem: {},
-  imageContainerGrid: {},
-  actionsGrid: {},
+  actionsGrid: {
+    paddingLeft: theme.spacing(3),
+    paddingRight: theme.spacing(2),
+  },
   button: {
     marginBottom: '0.5rem',
   },
@@ -72,7 +96,13 @@ const ProjectDetails = () => {
           item
           justifyContent="center"
           className={classes.imageContainerGrid}
-        ></Grid>
+        >
+          <Grid className={classes.imageGrid}>
+            <Paper className={classes.imagePaper}>
+              <InsertPhotoIcon className={classes.imageIcon} />
+            </Paper>
+          </Grid>
+        </Grid>
         <Grid
           xs={6}
           container
